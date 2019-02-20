e5b1d751110372f34924dbf3a2d4b7ae41a4a806
Zhehuai Chen <chenzhehuai.sjtu@aispeech.com>
2019-02-20 19:48:41 +0800

    add preinit by depth ; depth from 3 to 15, rtf from 1.8 to 1.4; fstinfo-init rtf 1.0; baseline 0.9

---
+++ bin/latgen-preinit-lazylm-faster-mapped.cc
@@ -0,0 +1,377 @@
+// bin/latgen-lazylm-faster-mapped.cc
+//
+// Copyright (c) 2016 Joan Puigcerver <joapuipe@gmail.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include "base/kaldi-common.h"
+#include "util/common-utils.h"
+#include "tree/context-dep.h"
+#include "hmm/transition-model.h"
+#include "fstext/fstext-lib.h"
+#include "decoder/decoder-wrappers.h"
+#include "decoder/decodable-matrix.h"
+#include "base/timer.h"
+#include <fst/script/info.h>
+
+namespace fst {
+
+template<class Arc>
+ComposeFst<Arc> TableComposeFst(
+    const Fst<Arc> &ifst1, const Fst<Arc> &ifst2,
+    const CacheOptions& cache_opts = CacheOptions(),
+    const int otf_mode = 1) {
+      if (otf_mode==1) {
+              typedef Fst<Arc> F;
+              typedef SortedMatcher<F> SM;
+              typedef TableMatcher<F> TM;
+              typedef ArcLookAheadMatcher<SM> LA_SM;
+              typedef SequenceComposeFilter<TM, LA_SM> SCF;
+              typedef LookAheadComposeFilter<SCF, TM, LA_SM, MATCH_INPUT> LCF;
+              typedef PushWeightsComposeFilter<LCF, TM, LA_SM, MATCH_INPUT> PWCF;
+              typedef PushLabelsComposeFilter<PWCF, TM, LA_SM, MATCH_INPUT> PWLCF;
+              TM* lam1 = new TM(ifst1, MATCH_OUTPUT);
+              LA_SM* lam2 = new LA_SM(ifst2, MATCH_INPUT);
+              PWLCF* laf = new PWLCF(ifst1, ifst2, lam1, lam2);
+              ComposeFstImplOptions<TM, LA_SM, PWLCF> opts(cache_opts, lam1, lam2, laf);
+              return ComposeFst<Arc>(ifst1, ifst2, opts);
+      }
+      else if (otf_mode==2) {
+              typedef LookAheadMatcher< StdFst > M;
+              typedef AltSequenceComposeFilter<M> SF;
+              typedef LookAheadComposeFilter<SF, M>  LF;
+              typedef PushWeightsComposeFilter<LF, M> WF;
+              typedef PushLabelsComposeFilter<WF, M> ComposeFilter;
+              typedef M FstMatcher;
+          
+              ComposeFstOptions<Arc, FstMatcher, ComposeFilter> opts2(cache_opts);
+              return ComposeFst<Arc>(ifst1, ifst2, opts2);
+      }
+      else if (otf_mode==3) {
+              typedef DefaultLookAhead<Arc, MATCH_OUTPUT> LA;
+              using FstMatcher = typename LA::FstMatcher;
+              using ComposeFilter = typename LA::ComposeFilter;
+              ComposeFstOptions<Arc, FstMatcher, ComposeFilter> opts3(cache_opts);
+              return ComposeFst<Arc>(ifst1, ifst2, opts3);
+      }
+      else if (otf_mode==4) {
+              typedef DefaultLookAhead<Arc, MATCH_INPUT> LA;
+              using FstMatcher = typename LA::FstMatcher;
+              using ComposeFilter = typename LA::ComposeFilter;
+              ComposeFstOptions<Arc, FstMatcher, ComposeFilter> opts3(cache_opts);
+              return ComposeFst<Arc>(ifst1, ifst2, opts3);
+      }
+      else if (otf_mode==5) {
+              return ComposeFst<Arc>(ifst1, ifst2, cache_opts);
+      }
+      else
+      {
+              KALDI_ERR << "otf_mode undefined: " << otf_mode << std::endl;
+              return ComposeFst<Arc>(ifst1, ifst2, cache_opts);
+      }
+}
+
+template <class Arc>
+void PreInitFSTByBFS(Fst<Arc> &fst, int32 depth = -1) {
+  using namespace kaldi;
+  using StateId = typename Arc::StateId;
+  
+  Timer timer;
+  std::set<StateId> visited;
+  std::queue<StateId> queue;
+  queue.push(fst.Start());
+  int32 cnt=0;
+  std::unordered_map<StateId, int32> levels;
+  levels[fst.Start()]=0;
+  visited.insert(fst.Start());
+  while (!queue.empty()) {
+    StateId state=queue.front();
+    queue.pop();
+    cnt++;
+    fst::ArcIterator<Fst<Arc>> aiter(fst, state);
+    for (; !aiter.Done(); aiter.Next()) {
+      const auto& arc = aiter.Value();
+      StateId nextstate = arc.nextstate;
+      if (visited.find(nextstate) == visited.end()) {
+        int32 new_lev = levels[state] + 1;
+        levels[nextstate] = new_lev;
+        visited.insert(nextstate);
+        if (new_lev <= depth || depth == -1) {
+          queue.push(nextstate);
+        }
+      }
+    }
+  }
+  KALDI_VLOG(0) << "preinit_info: " << timer.Elapsed() << " " << depth << " "<<cnt;
+}
+}  // namespace fst
+
+
+int main(int argc, char *argv[]) {
+  try {
+    using namespace kaldi;
+    typedef kaldi::int32 int32;
+    using fst::SymbolTable;
+    using fst::VectorFst;
+    using fst::StdArc;
+
+    const char *usage =
+        "Generate lattices, reading log-likelihoods as matrices and doing the "
+        "composition of HCL and G on-the-fly. You can use this decoder to "
+        "perform decoding using large grammars (i.e. large n-gram LMs).\n"
+        "\n"
+        "Usage:   latgen-lazylm-faster-mapped [options] <model-in> "
+        "<hcl-in> <g-in> <loglikes-rspecifier> <lattice-wspecifier> "
+        "[<words-wspecifier> [<alignments-wspecifier>]]\n";
+
+    ParseOptions po(usage);
+    Timer timer;
+    bool allow_partial = false;
+    BaseFloat acoustic_scale = 0.1;
+    int otf_mode=1;
+    LatticeFasterDecoderConfig config;
+    std::string word_syms_filename;
+    fst::CacheOptions cache_config;
+    int gc_limit = 536870912;  // 512MB
+    int preinit_mode=0;
+    BaseFloat preinit_para=-1;
+    int debug_mode=0;
+
+    config.Register(&po);
+    po.Register("acoustic-scale", &acoustic_scale,
+                "Scaling factor for acoustic likelihoos.");
+    po.Register("allow-partial", &allow_partial,
+                "If true, produce output even if end state was not reached.");
+    po.Register("compose-gc", &cache_config.gc,
+                "If false, any expanded state during the composition will be "
+                "cached. If true, the cache will be garbage collected when it "
+                "grows past --compose-gc-limit bytes.");
+    po.Register("compose-gc-limit", &gc_limit,
+                "Number of bytes allowed in the composition cache before "
+                "garbage collection.");
+    po.Register("word-symbol-table", &word_syms_filename,
+                "Symbol table for words [for debug output].");
+    po.Register("otf-mode", &otf_mode,
+                "on-the-fly comp. mode.");
+    po.Register("preinit-mode", &preinit_mode,
+                "preinit mode.");
+    po.Register("preinit-para", &preinit_para,
+                "preinit mode.");
+    po.Register("debug-mode", &debug_mode,
+                "on-the-fly debug mode.");
+
+
+    po.Read(argc, argv);
+    cache_config.gc_limit = gc_limit;
+
+    if (po.NumArgs() < 5 || po.NumArgs() > 7) {
+      po.PrintUsage();
+      exit(1);
+    }
+
+    std::string model_in_filename = po.GetArg(1),
+        hcl_in_str = po.GetArg(2),
+        g_in_str = po.GetArg(3),
+        feature_rspecifier = po.GetArg(4),
+        lattice_wspecifier = po.GetArg(5),
+        words_wspecifier = po.GetOptArg(6),
+        alignment_wspecifier = po.GetOptArg(7);
+
+    TransitionModel trans_model;
+    ReadKaldiObject(model_in_filename, &trans_model);
+
+    bool determinize = config.determinize_lattice;
+    CompactLatticeWriter compact_lattice_writer;
+    LatticeWriter lattice_writer;
+    if (! (determinize ? compact_lattice_writer.Open(lattice_wspecifier)
+           : lattice_writer.Open(lattice_wspecifier)))
+      KALDI_ERR << "Could not open table for writing lattices: "
+                 << lattice_wspecifier;
+
+    Int32VectorWriter words_writer(words_wspecifier);
+
+    Int32VectorWriter alignment_writer(alignment_wspecifier);
+
+    fst::SymbolTable *word_syms = NULL;
+    if (word_syms_filename != "")
+      if (!(word_syms = fst::SymbolTable::ReadText(word_syms_filename)))
+        KALDI_ERR << "Could not read symbol table from file "
+                   << word_syms_filename;
+
+    double tot_like = 0.0, elapsed=0.0, remain_elapsed=0.0, remain_elapsed_st;
+    kaldi::int64 frame_count = 0;
+    int num_success = 0, num_fail = 0;
+
+    const bool is_table_hcl =
+        ClassifyRspecifier(hcl_in_str, NULL, NULL) != kNoRspecifier;
+    const bool is_table_g =
+        ClassifyRspecifier(g_in_str, NULL, NULL) != kNoRspecifier;
+
+    if (!is_table_hcl && !is_table_g) {
+      SequentialBaseFloatMatrixReader loglike_reader(feature_rspecifier);
+      // It's important that we initialize decode_fst after loglikes_reader,
+      // as it can prevent crashes on systems installed without enough virtual
+      // memory.
+      // It has to do with what happens on UNIX systems if you call fork() on a
+      // large process: the page-table entries are duplicated, which requires a
+      // lot of virtual memory.
+      fst::Fst<StdArc> *hcl_fst = fst::ReadFstKaldiGeneric(hcl_in_str, true);
+      fst::Fst<StdArc> *g_fst = fst::ReadFstKaldiGeneric(g_in_str, true);
+
+      // On-demand composition of HCL and G
+      fst::ComposeFst<StdArc> decode_fst = fst::TableComposeFst(
+                  *hcl_fst, *g_fst, cache_config, otf_mode);
+      if (debug_mode&(1<<2))
+      {
+        timer.Reset();
+        // delete these only after decoder goes out of scope.
+        fst::FstInfo fstinfo(decode_fst,false, "any", "long");
+        fst::PrintFstInfoImpl(fstinfo, true);
+        remain_elapsed_st = timer.Elapsed();
+      }
+
+      if (preinit_mode) {
+        switch (preinit_mode) {
+          case 1:
+            fst::PreInitFSTByBFS(decode_fst, (int32)preinit_para);
+            break;
+          default:
+            break;
+        }
+      }
+
+      timer.Reset();
+      {
+        LatticeFasterDecoder decoder(decode_fst, config);
+
+        for (; !loglike_reader.Done(); loglike_reader.Next()) {
+          std::string utt = loglike_reader.Key();
+          Matrix<BaseFloat> loglikes (loglike_reader.Value());
+          loglike_reader.FreeCurrent();
+          if (loglikes.NumRows() == 0) {
+            KALDI_WARN << "Zero-length utterance: " << utt;
+            num_fail++;
+            continue;
+          }
+
+          DecodableMatrixScaledMapped decodable(trans_model, loglikes,
+                                                acoustic_scale);
+
+          double like;
+          if (DecodeUtteranceLatticeFaster(
+                  decoder, decodable, trans_model, word_syms, utt,
+                  acoustic_scale, determinize, allow_partial, &alignment_writer,
+                  &words_writer, &compact_lattice_writer, &lattice_writer,
+                  &like)) {
+            tot_like += like;
+            frame_count += loglikes.NumRows();
+            num_success++;
+          } else num_fail++;
+    if (debug_mode&(1<<1))
+    {
+        ;
+        //std::cout << "arc state:"<<  decode_fst.fst::internal::CacheImpl::NumStates(); //decode_fst.GetMutableImpl()->fst::internal::ComposeFstImplBase<StdArc>::NumArcs() <<
+    }
+        }
+      }
+    elapsed = timer.Elapsed();
+    if (debug_mode&(1<<0))
+    {
+      timer.Reset();
+      // delete these only after decoder goes out of scope.
+      fst::FstInfo fstinfo(decode_fst,false, "any", "long");
+      fst::PrintFstInfoImpl(fstinfo, true);
+      remain_elapsed = timer.Elapsed();
+    }
+
+      delete hcl_fst;
+      delete g_fst;
+    } 
+#if 0
+    else if(is_table_hcl && is_table_g) {
+      // We have different FSTs for different utterances.
+      SequentialBaseFloatMatrixReader loglike_reader(feature_rspecifier);
+      RandomAccessTableReader<fst::VectorFstHolder> hcl_reader(hcl_in_str);
+      RandomAccessTableReader<fst::VectorFstHolder> g_reader(g_in_str);
+      for (; !loglike_reader.Done(); loglike_reader.Next()) {
+        std::string utt = loglike_reader.Key();
+        const Matrix<BaseFloat> &loglikes = loglike_reader.Value();
+        loglike_reader.FreeCurrent();
+        if (loglikes.NumRows() == 0) {
+          KALDI_WARN << "Zero-length utterance: " << utt;
+          num_fail++;
+          continue;
+        }
+        if (!hcl_reader.HasKey(utt)) {
+          KALDI_WARN << "Not decoding utterance " << utt
+                     << " because no HCL is available.";
+          num_fail++;
+          continue;
+        }
+        if (!g_reader.HasKey(utt)) {
+          KALDI_WARN << "Not decoding utterance " << utt
+                     << " because no G is available.";
+          num_fail++;
+          continue;
+        }
+
+        // On-demand composition of HCL and G
+        fst::ComposeFst<StdArc> decode_fst = fst::TableComposeFst(
+            hcl_reader.Value(utt), g_reader.Value(utt), cache_config);
+
+        LatticeFasterDecoder decoder(decode_fst, config);
+        DecodableMatrixScaledMapped decodable(trans_model, loglikes,
+                                              acoustic_scale);
+        double like;
+        if (DecodeUtteranceLatticeFaster(
+                decoder, decodable, trans_model, word_syms, utt, acoustic_scale,
+                determinize, allow_partial, &alignment_writer, &words_writer,
+                &compact_lattice_writer, &lattice_writer, &like)) {
+          tot_like += like;
+          frame_count += loglikes.NumRows();
+          num_success++;
+        } else num_fail++;
+      }
+    } 
+#endif    
+    else {
+      KALDI_ERR << "The decoding of tables/non-tables and match-type that you "
+                << "supplied is not currently supported. Either implement "
+                << "this, ask the maintainers to implement it, or call this "
+                << "program differently.";
+    }
+
+
+    KALDI_LOG << "Time taken "<< elapsed
+              << "s: real-time factor assuming 100 frames/sec is "
+              << (elapsed*100.0/frame_count);
+    KALDI_LOG << "loading remaining HCLG: "<< remain_elapsed_st <<" "<< remain_elapsed ;
+    KALDI_LOG << "Done " << num_success << " utterances, failed for "
+              << num_fail;
+    KALDI_LOG << "Overall log-likelihood per frame is " << (tot_like/frame_count) << " over "
+              << frame_count<<" frames.";
+
+    delete word_syms;
+    if (num_success != 0) return 0;
+    else return 1;
+  } catch(const std::exception &e) {
+    std::cerr << e.what();
+    return -1;
+  }
+}
---
+++ scripts/run2c.ad.sh
@@ -0,0 +1,102 @@
+. path.sh
+. cmd.sh
+
+set -x
+#otfdec=decode-lazylm-faster-mapped
+otfdec=latgen-preinit-lazylm-faster-mapped
+decode_cmd=$decode_cmd" -l hostname=c*  " #-l hostname=c02
+stage=4
+otf_data=data_otf/lang_test_tgsmall.h3/
+#otf_data=data_otf/lang_test_tgsmall.h/
+affix=1g   # affix for the TDNN directory name
+amdir=exp/chain/tdnn${affix}_sp/
+
+
+
+if [ $stage -le 0 ]; then
+bash scripts/makehlevel_3.sh data/lang_test_tgsmall  exp/chain/tdnn${affix}_sp/ $otf_data $KALDI_ROOT #exp/chain/tree_sp/graph_tgsmall
+
+exit
+fi
+
+if [ $stage -le 4 ]; then
+chunk_left_context=0
+chunk_right_context=0
+chunk_width=140,100,160
+frames_per_chunk=$(echo $chunk_width | cut -d, -f1)
+nnet3_affix=
+tree_affix=
+tree_dir=exp/chain${nnet3_affix}/tree_sp${tree_affix:+_$tree_affix}
+data=dev_clean_2
+nspk=$(wc -l <data/${data}_hires/spk2utt)
+dir=exp/chain${nnet3_affix}/tdnn${affix}_sp
+otflang=$otf_data
+
+false && \
+{
+      decgraph=$otflang/HcCLG.fst
+      #decgraph=$otflang/HcCLG2.fst
+      scripts/decode.otf.sh \
+          --decgraph $decgraph \
+          --acwt 1.0 --post-decode-acwt 10.0 \
+          --extra-left-context $chunk_left_context \
+          --extra-right-context $chunk_right_context \
+          --extra-left-context-initial 0 \
+          --extra-right-context-final 0 \
+          --frames-per-chunk $frames_per_chunk \
+          --nj $nspk --cmd "$decode_cmd"  --num-threads 1 \
+          --online-ivector-dir exp/nnet3${nnet3_affix}/ivectors_${data}_hires \
+          --amsplit true \
+          $tree_dir/graph_tgsmall/ data/${data}_hires ${dir}/decode_tgsmall_${data}.amsp.`basename $otflang``basename $decgraph` \
+          || exit 1
+      }
+false && \
+{
+      decgraph=$otflang/HCLG.fst
+      #decgraph=$otflang/HcCLG2.fst
+      scripts/decode.otf.sh \
+          --decgraph $decgraph \
+          --acwt 1.0 --post-decode-acwt 10.0 \
+          --extra-left-context $chunk_left_context \
+          --extra-right-context $chunk_right_context \
+          --extra-left-context-initial 0 \
+          --extra-right-context-final 0 \
+          --frames-per-chunk $frames_per_chunk \
+          --nj $nspk --cmd "$decode_cmd"  --num-threads 1 \
+          --online-ivector-dir exp/nnet3${nnet3_affix}/ivectors_${data}_hires \
+          --amsplit true \
+          $tree_dir/graph_tgsmall/ data/${data}_hires ${dir}/decode_tgsmall_${data}.amsp.`basename $otflang``basename $decgraph` \
+          || exit 1
+      }&
+
+
+#false && \
+    {
+for dec_conf in conf/otf.2b.conf #conf/otf.1b.conf 
+do
+    for otf_mode in 3 #2
+    do
+    {
+      scripts/decode.otf.sh \
+          --config $dec_conf \
+          --stage 0 \
+          --acwt 1.0 --post-decode-acwt 10.0 \
+          --extra-left-context $chunk_left_context \
+          --extra-right-context $chunk_right_context \
+          --extra-left-context-initial 0 \
+          --extra-right-context-final 0 \
+          --frames-per-chunk $frames_per_chunk \
+          --nj $nspk --cmd "$decode_cmd"  --num-threads 1 \
+          --online-ivector-dir exp/nnet3${nnet3_affix}/ivectors_${data}_hires \
+          --otf_addin " --otf-mode=$otf_mode " \
+          --otfdec $otfdec \
+          $otflang data/${data}_hires ${dir}/decode_tgsmall_${data}.`basename $otflang`.$otf_mode.`basename $dec_conf`.$otfdec \
+          || exit 1
+      }&
+  done
+  done
+}
+
+  wait
+
+fi
